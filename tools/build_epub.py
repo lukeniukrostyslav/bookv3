03e6ae2a1c2b941d5e28233d53632413ea47e10a
lukeniukrostyslav <lukeniukrostyslav@gmail.com>
2026-09-20 23:13:07 +0200

    B14.9: harden EPUB metadata for EPUBCheck

--- tools/build_epub.py
+++ tools/build_epub.py
@@ -20,14 +20,16 @@ for i in range(1,33):
     paragraphs=[]
     for block in re.split(r"\n\s*\n", body):
         block=block.strip()
-        if not block: continue
-        if block.startswith("# "): continue
+        if not block:
+            continue
+        if block.startswith("# "):
+            continue
         paragraphs.append(f"<p>{html.escape(block).replace(chr(10), '<br/>')}</p>")
     chapters.append((i,title,"\n".join(paragraphs)))
 
 book_title="ОТЕЛЬ ПОСЛЕДНЕГО ЗАВТРА"
 author="РОСТИСЛАВ ЛУКЕНЮК"
-uid="hotel-last-tomorrow-rostyslav-lukeniuk"
+uid="urn:uuid:7d9d8f8e-4f1d-4e3d-9b0a-5e7c2a9d1f32"
 
 css="""body{font-family:serif;line-height:1.5;margin:8%;}h1{text-align:center;margin-top:25%;}p{text-indent:1.2em;margin:0 0 .7em;}nav ol{list-style:none;padding:0;}nav li{margin:.5em 0;}"""
 
@@ -39,22 +41,23 @@ with zipfile.ZipFile(OUT/"HOTEL_LAST_TOMORROW.epub","w") as z:
 </container>'''
     z.writestr("META-INF/container.xml",container)
     z.writestr("EPUB/style.css",css)
-    manifest=[]; spine=[]
+    manifest=[]
+    spine=[]
     for i,title,body in chapters:
         fn=f"chapter-{i:02d}.xhtml"
         doc=f'''<?xml version="1.0" encoding="UTF-8"?>
-<html xmlns="http://www.w3.org/1999/xhtml"><head><title>{html.escape(title)}</title><link rel="stylesheet" type="text/css" href="style.css"/></head><body><h1>{html.escape(title)}</h1>{body}</body></html>'''
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru" xml:lang="ru"><head><title>{html.escape(title)}</title><link rel="stylesheet" type="text/css" href="style.css"/></head><body><h1>{html.escape(title)}</h1>{body}</body></html>'''
         z.writestr("EPUB/"+fn,doc)
         manifest.append(f'<item id="c{i}" href="{fn}" media-type="application/xhtml+xml"/>')
         spine.append(f'<itemref idref="c{i}"/>')
     nav_items="".join(f'<li><a href="chapter-{i:02d}.xhtml">{html.escape(t)}</a></li>' for i,t,_ in chapters)
     nav=f'''<?xml version="1.0" encoding="UTF-8"?>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><title>Содержание</title></head><body><nav epub:type="toc"><h1>Содержание</h1><ol>{nav_items}</ol></nav></body></html>'''
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="ru" xml:lang="ru"><head><title>Содержание</title></head><body><nav epub:type="toc"><h1>Содержание</h1><ol>{nav_items}</ol></nav></body></html>'''
     z.writestr("EPUB/nav.xhtml",nav)
     manifest.append('<item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/>')
     opf=f'''<?xml version="1.0" encoding="UTF-8"?>
 <package xmlns="http://www.idpf.org/2007/opf" version="3.0" unique-identifier="bookid">
 <metadata xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:identifier id="bookid">{uid}</dc:identifier><dc:title>{book_title}</dc:title><dc:language>ru</dc:language><dc:creator>{author}</dc:creator><meta property="dcterms:modified">2026-09-20T00:00:00Z</meta></metadata>
-<manifest>{''.join(manifest)}</manifest><spine>{''.join(spine)}</spine></package>'''
+<manifest>{''.join(manifest)}</manifest><spine page-progression-direction="ltr">{''.join(spine)}</spine></package>'''
     z.writestr("EPUB/content.opf",opf)
 print(OUT/"HOTEL_LAST_TOMORROW.epub")
