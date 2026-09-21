381badf538c64494d914faef96708a8e2ca22581
lukeniukrostyslav <lukeniukrostyslav@gmail.com>
2026-09-21 13:40:30 +0200

    fix: harden Italian EPUB metadata build

--- tools/build_epub_it.py
+++ tools/build_epub_it.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import html, re, zipfile
 from pathlib import Path
+from datetime import datetime, timezone
 ROOT=Path(__file__).resolve().parents[1]
 MANUSCRIPT=ROOT/"manuscript_it"
 OUT=ROOT/"dist"; OUT.mkdir(exist_ok=True)
@@ -16,10 +17,11 @@ for i in range(1,33):
         block=block.strip()
         if block: paragraphs.append(f"<p>{html.escape(block).replace(chr(10), '<br/>')}</p>")
     chapters.append((i,title,"\n".join(paragraphs)))
-book_title="L'HOTEL DELL'ULTIMO DOMANI"
+book_title="L'Hotel dell'ultimo domani"
 author="ROSTYSLAV LUKENIUK"
 uid="urn:uuid:7d9d8f8e-4f1d-4e3d-9b0a-5e7c2a9d1f33"
 css="body{font-family:serif;line-height:1.5;margin:8%;}h1{text-align:center;margin-top:25%;}p{text-indent:1.2em;margin:0 0 .7em;}nav ol{list-style:none;padding:0;}nav li{margin:.5em 0;}"
+modified=datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00","Z")
 with zipfile.ZipFile(OUT/"HOTEL_LAST_TOMORROW_IT.epub","w") as z:
     z.writestr("mimetype","application/epub+zip",compress_type=zipfile.ZIP_STORED)
     z.writestr("META-INF/container.xml",'<?xml version="1.0" encoding="UTF-8"?><container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container"><rootfiles><rootfile full-path="EPUB/content.opf" media-type="application/oebps-package+xml"/></rootfiles></container>')
@@ -32,6 +34,6 @@ with zipfile.ZipFile(OUT/"HOTEL_LAST_TOMORROW_IT.epub","w") as z:
     nav_items="".join(f'<li><a href="chapter-{i:02d}.xhtml">{html.escape(t)}</a></li>' for i,t,_ in chapters)
     z.writestr("EPUB/nav.xhtml",f'<?xml version="1.0" encoding="UTF-8"?><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="it" xml:lang="it"><head><title>Indice</title></head><body><nav epub:type="toc"><h1>Indice</h1><ol>{nav_items}</ol></nav></body></html>')
     manifest.append('<item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav"/>')
-    opf=f'<?xml version="1.0" encoding="UTF-8"?><package xmlns="http://www.idpf.org/2007/opf" version="3.0" unique-identifier="bookid"><metadata xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:identifier id="bookid">{uid}</dc:identifier><dc:title>{html.escape(book_title)}</dc:title><dc:language>it</dc:language><dc:creator>{html.escape(author)}</dc:creator><meta property="dcterms:modified">2026-09-21T00:00:00Z</meta></metadata><manifest>{"".join(manifest)}</manifest><spine page-progression-direction="ltr">{"".join(spine)}</spine></package>'
+    opf=f'<?xml version="1.0" encoding="UTF-8"?><package xmlns="http://www.idpf.org/2007/opf" version="3.0" unique-identifier="bookid"><metadata xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:identifier id="bookid">{uid}</dc:identifier><dc:title>{html.escape(book_title)}</dc:title><dc:language>it</dc:language><dc:creator>{html.escape(author)}</dc:creator><meta property="dcterms:modified">{modified}</meta></metadata><manifest>{"".join(manifest)}</manifest><spine page-progression-direction="ltr">{"".join(spine)}</spine></package>'
     z.writestr("EPUB/content.opf",opf)
 print(OUT/"HOTEL_LAST_TOMORROW_IT.epub")
